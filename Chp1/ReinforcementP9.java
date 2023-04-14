f55945ec82ae416c99c50316f475e087a3ca3207
n00biex141 <n00biex141@gmail.com>
2023-04-14 13:31:05 +0500

    All Reinforcement Done

--- Chp1/ReinforcementP9.java
+++ Chp1/ReinforcementP9.java
@@ -6,5 +6,26 @@ of each type, and getting the value of each type.
 */
 
 public class ReinforcementP9{
-    
+    Flower flr1 = new Flower("Red Rose", 10.0, 6);
+    Flower flr2 = new Flower("White Rose", 12.0, 8);
+}
+
+class Flower{
+    private String name;
+    private double price;
+    private int petals;
+
+    public Flower(String name, double price, int petals){
+        this.name = name;
+        this.price = price;
+        this.petals = petals;
+    }
+
+    public void setName(String name){ this.name = name; }
+    public void setPrice(double price){ this.price = price; }
+    public void setPetals(int petals){ this.petals = petals; }
+
+    public String getName(){ return name; }
+    public double getPrice(){ return price; }
+    public int getPetals(){ return petals; }
 }
