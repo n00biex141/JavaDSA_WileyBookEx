cc2e9e07bdee9854736503a9a78e17044145a2e4
n00biex141 <n00biex141@gmail.com>
2023-04-14 12:43:16 +0500

    solved

--- Chp1/ReinforcementP6.java
+++ Chp1/ReinforcementP6.java
@@ -4,5 +4,14 @@ than or equal to n.
 */
 
 public class ReinforcementP6{
-    
+    public static void main(String[] args){
+        System.out.print(allSquareNum(4));
+    }
+
+    public static int allSquareNum(int n){
+        int sum = 0;
+        for(int i = 1; i <= n; i++)
+            sum += Math.pow(i, 2);
+        return sum;
+    }
 }
