ad9faf2774c3e553da688907efb8af07e587887a
n00biex141 <n00biex141@gmail.com>
2023-04-14 10:35:41 +0500

    P1

--- Chp1/ReinforcementP1.java
+++ Chp1/ReinforcementP1.java
@@ -1,5 +1,44 @@
+/*
+Suppose that we create an array A of GameEntry objects, which has an
+integer scores field, and we clone A and store the result in array B
+If we immediately set A[4].score equal to 550, what is the score value
+of the GameEntry object referenced by B[4]?
+*/
+
 public class ReinforcementP1{
     public static void main(String[] args){
-        
+
+        GameEntry[] arrayA = {
+            new GameEntry(10),
+            new GameEntry(14),
+            new GameEntry(15),
+            new GameEntry(11),
+            new GameEntry(13),
+        };
+
+        GameEntry[] arrayB = arrayA.clone();
+
+        arrayA[4].setScore(550);
+
+        System.out.print(arrayB[4].getScore());
+
+        /*
+        If we clone an array of GameEntry objects,
+        a new array with the same size and elements is created, 
+        but the elements themselves are not cloned. 
+        That means that both arrays will contain references to the same GameEntry objects in memory.
+        */
     }
-} 
+}
+
+class GameEntry{
+    private int score;
+
+    public GameEntry(int score){
+        this.score = score;
+    }
+
+    public int getScore(){ return score; }
+
+    public void setScore(int score) { this.score = score; }
+}
